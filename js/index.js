9bbc67a37f64410a1e114ddf8963dd6c6dd4a2ce
fatma2604 <132596038+fatma2604@users.noreply.github.com>
2023-05-05 22:33:37 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+$('.slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll:3,
+    adaptiveHeight: true,
+    nextArrow:'<i class="fa-solid fa-arrow-right" style="color: #000000;"></i>',
+    prevArrow:'<i class="fa-solid fa-arrow-left" style="color: #000000;"></i>'
+  });
